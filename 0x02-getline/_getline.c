46a4627a3655e592aacf7790acb35d73c2505492
johndspence <johndspence@gmail.com>
2017-07-10 03:56:33 +0000

    Looking for edge cases

--- 0x02-getline/_getline.c
+++ 0x02-getline/_getline.c
@@ -14,6 +14,7 @@ void *_realloc(void *ptr, size_t size)
 	char *new_ret_ln;
 
 	new_ret_ln = malloc(sizeof(char) * size);
+	memset
 	if (new_ret_ln)
 	{
 		memcpy(new_ret_ln, ptr, size - 1);
@@ -43,7 +44,7 @@ char *_getline(const int fd)
 
 	ret_ln_sz = BUF_SIZE;
 	ret_ln = malloc(sizeof(char) * ret_ln_sz);
-	if (ret_ln == NULL)
+	if (fd == -1 || ret_ln == NULL)
 	{
 		return (NULL);
 	}
